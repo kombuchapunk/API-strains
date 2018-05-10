b47b536a046684c7cd4bdf7bbbb6813fccf04940
Student <example@example.com>
2018-05-10 16:12:56 -0700

    fix migration

--- db/migrate/20180509202859_add_ratings_table.rb
+++ db/migrate/20180509202859_add_ratings_table.rb
@@ -1,7 +1,7 @@
 class AddRatingsTable < ActiveRecord::Migration[5.2]
   def change
     create_table :ratings do |t|
-      t.decimal :rating, :decimal :precision => 2, :scale => 1
+      t.decimal :rating, :decimal, :precision => 2, :scale => 1
       t.integer :strain_id
 
       t.timestamps
