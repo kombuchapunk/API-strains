db53493b93cc863e921c74e4cfcf89b690e421e2
Student <example@example.com>
2018-05-10 16:50:18 -0700

    switch host to heroku

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -46,7 +46,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
 
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'strain-api.herokuapp.com' }
 
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
