28ff6e80ede26b37c0851ad9b973e6dbe4f119ab
Student <example@example.com>
2018-05-10 16:38:41 -0700

    get rid of authentication controller conditional and move it to users controller

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,14 +1,5 @@
 class AuthenticationController < ApplicationController
   skip_before_action :authenticate_request
 
-  def authenticate
 
-    command = AuthenticateUser.call(params[:email], params[:password])
-
-    if command.success?
-      render json: { auth_token: command.result instructions: "Use this as a header labeled authorization in your future requests"}
-    else
-      render json: { error: command.errors }, status: :unauthorized
-    end
-  end
 end
